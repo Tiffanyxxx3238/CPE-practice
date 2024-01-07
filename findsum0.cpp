a0b56b08c174ae31db33c201c48f61a1f85d43ca
Tiffany Chen <83951551+Tiffanyxxx3238@users.noreply.github.com>
2024-01-07 15:39:27 +0800

    add 2024/01/07 practice by C語言用資料結構與演算法
    從一系列以排序的數字中，找出兩者和為0

---
+++ findsum0.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+	int myNum[8] = {-7, -5, -3,-1,2,4,5,8};
+	for(int k=0;k<=7;k++){
+		for(int i=k+1;i<=7;i++){
+			if(myNum[k]+myNum[i]==0){
+				cout<<myNum[k]<<"+"<<myNum[i]<<"©M¬°0\n";
+			}
+			else{
+			}
+	}
+	}
+	return 0;
+} 
